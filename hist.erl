4bce1691b18ec7d75b85da7325425aff360df3a1
hph <hans-ph@web.de>
2023-05-29 17:32:58 +0200

    updated hist.erl and extended unit tests

--- hist.erl
+++ hist.erl
@@ -1,93 +1,132 @@
 -module(hist).
 
--export( [ create/4 ] ).
+-export( [ create/2, create/4, print/1 ] ).
 
 -include_lib("eunit/include/eunit.hrl").
 
--record( histogram, { min, max, nbins, binsize, counters } ).
+-record( histogram, { min, max, nbins, binsize, frequency } ).
+
+-type histogram() :: #histogram{}.
+
+-export_type( [histogram/0] ).
 
 
 %
-% @doc create a histogram.
+% @param Numbers : a list containing the data
+% @param Bins : number of bins, starting from min(Numbers) to max(Numbers)
 %
-% @param Numbers is a list of numbers
-% @param Bins is the number of slots (bins)
-% @param Min is the smallest number
-% @param Max is the largest number
+-spec create( list(number()), non_neg_integer() ) -> histogram().
+create( Numbers, Bins ) ->
+	create(Numbers,Bins,lists:min(Numbers),lists:max(Numbers)).
+
 %
-% @returns a histogram as a list
+% @param Numbers : a list containing the data
+% @param Bins : number of bins
+% @param Min : minimum, "left" side of the histogram, everything small than Min is counted in the first bin
+% @param Max : maximum, "right" side, everything larger is counted in the last bin
 %
--spec create( list(number()), non_neg_integer(), number(), number() ) -> list().
+-spec create( list(number()), non_neg_integer(), number(), number() ) -> histogram().
 create( Numbers, Bins, Min, Max ) ->
-	Bs = abs((Max-Min)/Bins),
 	H = #histogram{
-		binsize = Bs,
 		min = Min,
-		max = Max,
-		counters = counters:new(Bins,[atomics]),
-		nbins = Bins
+		nbins = Bins,
+		binsize = abs((Max-Min)/Bins),
+		max = Max - abs((Max-Min)/Bins),
+		frequency = []
 	},
-	?debugFmt("Histarray Bins=~p, Min=~p, Max=~p, Binsize=~p~n",[Bins,Min,Max,H#histogram.binsize]),
-	carray_tolist(hist(Numbers,H)).
+	Hc = counters:new(Bins,[atomics]),
+	hist(Numbers,H,Hc).
 
+-spec print( histogram() ) -> ok.
+print( H ) ->
+	io:format("Histarray Bins=~p, Min=~p, Max=~p, Binsize=~p~n",[H#histogram.nbins,H#histogram.min,H#histogram.max,H#histogram.binsize]),
+	?debugFmt("Histarray Bins=~p, Min=~p, Max=~p, Binsize=~p",[H#histogram.nbins,H#histogram.min,H#histogram.max,H#histogram.binsize]),
+	print(H#histogram.nbins, H).
+
+print(0,_) -> ok;
+
+print( I, H ) ->
+	Lower = H#histogram.min + (I-1.0)*H#histogram.binsize,
+	Upper = Lower + H#histogram.binsize,
+	io:format("~p: Val ~f < ~f : ~p~n",[I,Lower, Upper, lists:nth(I,H#histogram.frequency)]),
+	?debugFmt("~p: Val ~f < ~f : ~p",[I,Lower, Upper, lists:nth(I,H#histogram.frequency)]),
+	print(I-1,H).
 
 %
 % ------- private -------
 %
 
 % counters to list
+-spec carray_tolist(counters:counters_ref()) -> list().
 carray_tolist(Arr) ->
-	Inf=counters:info(Arr#histogram.counters),
+	Inf=counters:info(Arr),
 	Size=maps:get(size,Inf),
 	carray_tolist(Arr, Size, [] ).
 
 carray_tolist(_,0,List) -> List;
 
 carray_tolist(Arr,ArrIter,List) ->
-	carray_tolist( Arr, ArrIter-1, [ counters:get(Arr#histogram.counters,ArrIter) | List ] ).
+	carray_tolist( Arr, ArrIter-1, [ counters:get(Arr,ArrIter) | List ] ).
 
 %
 % create histogram from list
 %
-hist([],Histarray) ->
-	Histarray;
+-spec hist(list(), histogram(), counters:counters_ref()) -> histogram().
+hist([],H,C) ->
+	H#histogram{frequency = carray_tolist(C)};
 
-hist([Head|Tail],Histarray) ->
+hist([Head|Tail],H,C) ->
 	if
-		Head =< Histarray#histogram.min ->
-			counters:add(Histarray#histogram.counters, 1, 1);
-		Head >= Histarray#histogram.max ->
-			counters:add(Histarray#histogram.counters, Histarray#histogram.nbins,1);
+		Head < (H#histogram.min + H#histogram.binsize) ->
+			%io:format("inc min bin 1~n"),
+			counters:add(C, 1, 1);
+		Head > H#histogram.max ->
+			%io:format("inc bin ~p~n",[index(Histarray#histogram.max,Histarray)]),
+			counters:add(C, H#histogram.nbins,1);
 		true ->
-			X = index(Head,Histarray),
-			?debugFmt("~p inc bin[~p]~n",[Head,X]),
-			counters:add(Histarray#histogram.counters, X, 1)
+			X = index(Head,H),
+			%io:format("inc bin ~p~n",[X]),
+			counters:add(C, X, 1)
 	end,
-	hist(Tail,Histarray).
+	hist(Tail,H,C).
 
 
 index(Val,H) ->
-	%trunc(abs((Val - H#histogram.min)/H#histogram.binsize)).
-	trunc( Val/H#histogram.binsize ) - H#histogram.min.
+	trunc(abs((Val - H#histogram.min)/H#histogram.binsize)) +1.
 
 % ----------- unit test -----------
+
+assertHist( Expected, Hist ) ->
+	?debugFmt("--- Input: ~p",[Expected]),
+%	print(Hist),
+	?assertEqual( Expected, Hist#histogram.frequency ),
+	?assertEqual( length(Expected), Hist#histogram.nbins ),
+	?assertEqual( length(Hist#histogram.frequency), Hist#histogram.nbins ).
+
+% check if we counted every element; N shall be the length of the original data vector
+assertNum(N,H) ->
+	?assertEqual( N, lists:sum(H#histogram.frequency) ).
+
 hist_test() ->
-	Nums = [1,2,3,4,4,5,5,4,3],
-	H = create(Nums,10,0,10),
-	io:format("~p~n",[H]),
-	?assertEqual( [1,1,2,3,2,0,0,0,0,0],H ),
+	assertHist( [0,0,0,0,0], create([],5,0,10) ),
+	assertNum( 0,create([],5,0,10)),
 
-	?assertEqual( [0,0,0,0,0,1,1,2,3,2], create(Nums,10,-5,5) ),
-	?assertEqual( [1,0,0,1,0,2,0,0,3,2], create(Nums,10,1,5) ),
+	assertHist( [1,0,0,0,0], create([1],5,0,10) ),
+	assertNum( 1,create([1],5,0,10)),
+	assertHist( [0,1,0,0,0], create([2],5,0,10) ),
 
-	%N2 = [-2,3,3,3.33,0,0,4.5,9,12312],
-	%io:fwrite("Zahlen: ~w\n", [N2]),
-	%print(create(N2,10,0,10)),
-	%print(create(N2,10,-5,5)),
+	% outliers
+	assertHist( [1,0,0,0,1], create([-1,99],5,0,10) ),
 
-	%N3 = [rand:normal() || _ <- lists:seq(1, 100000)],
-	%stat:print( stat:minmax(N3) ),
-	
-	%io:fwrite("Zahlen: ~w\n", [N3]),
-	%o:format("~p~n",[create(N3,10,-10,10)] ).
+	% https://www.crashkurs-statistik.de/histogramme/
+	H2 = [172,164,160,162,173,180,158,185,171,181,162,184,177,175,177,174,158,151,192,177],
+	Hist = create(H2,5,150,200),
+	assertHist( [3,4,8,4,1],Hist),
+
+	% https://www.wallstreetmojo.com/histogram-examples/
+	Wtimes = [2.3,5,3.55,2.5,5.1,4.21,3.33,4.1,2.55,5.07,3.45,4.1,5.12],
+	Whist = create(Wtimes,5),
+%	print(Whist),
+	assertHist([3,1,2,3,4],Whist),
+
 	ok.
